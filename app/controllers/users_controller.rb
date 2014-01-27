754aac1280915781e56dc9b9257a4302e7c110d7
Willian <willian@pythonheads.nl>
2014-01-27 21:25:47 +0100

    Added update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
+# -*- encoding : utf-8 -*-
+
 class UsersController < ApplicationController
+  before_filter :load_user, only: [:update]
+  
   def create
     user = User.new user_params
     if user.save
@@ -8,7 +12,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update user_params
+      render json: @user
+    else
+      render status: :bad_request, json: {errors: @user.errors}
+    end
+  end
+
   private
+  def load_user
+    @user = User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:identifier).tap do |whitelisted|
       whitelisted[:properties] = params[:user][:properties]
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,26 +1,45 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = create :user
+  end
+
   def json_response
     JSON.parse(response.body)
   end
 
-  # Create an user 
+  def user_attributes
+    {identifier: 'test@example.com', properties: {favorite_color: 'red'}}
+  end
 
+  # Create an user 
   test "create a user with identifier" do
-    get :create, format: :json, user: {identifier: 'test@example.com'}
+    post :create, format: :json, user: user_attributes
     assert_equal 'test@example.com', json_response['user']['identifier']
   end
 
   test "create a user with properties" do
-    get :create, format: :json, user: {identifier: 'test@example.com', properties: {favorite_color: 'red'}}
+    post :create, format: :json, user: user_attributes
     assert_equal({'favorite_color' => 'red'}, json_response['user']['properties'])
   end
 
   # Errors when creating a user
-
   test "create a user without identifier" do
-    get :create, format: :json, user: {identifier: nil}
+    post :create, format: :json, user: {identifier: nil}
+    assert_response :bad_request
+    assert json_response["errors"].has_key? "identifier"
+  end
+
+  # Update an user
+  test "update an user" do
+    put :update, format: :json, id: @user.id, user: {identifier: 'user@example.com'}
+    assert_equal 'user@example.com', json_response['user']['identifier']
+  end
+
+  # Update an user with error
+  test "update a user without identifier" do
+    put :update, format: :json, id: @user.id, user: {identifier: nil}
     assert_response :bad_request
     assert json_response["errors"].has_key? "identifier"
   end
