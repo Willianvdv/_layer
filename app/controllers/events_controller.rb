fc3fedf8c3ee103c0a089a245387887437276253
Willian van der Velde <willian@pythonheads.nl>
2014-05-13 22:37:22 +0200

    More Dry for controllers

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,3 @@
 # -*- encoding : utf-8 -*-
 
-class EventsController < ApplicationController
-  def create
-    event = Event.new event_params
-    if event.save
-      render json: event
-    else
-      render status: :bad_request, json: { errors: event.errors }
-    end
-  end
-end
+class EventsController < ResourceController; end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,5 @@
 # -*- encoding : utf-8 -*-
 
-class UsersController < ApplicationController
-  before_filter :load_user, only: [:update]
-
-  def create
-    user = User.new user_params
-    if user.save
-      render json: user
-    else
-      render status: :bad_request, json: { errors: user.errors }
-    end
-  end
-
-  def update
-    if @user.update user_params
-      render json: @user
-    else
-      render status: :bad_request, json: { errors: @user.errors }
-    end
-  end
-
-  private
-
-  def load_user
-    @user = User.find params[:id]
-  end
+class UsersController < ResourceController
+  before_filter :load_resource, only: [:update]
 end
