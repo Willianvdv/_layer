a51cd7b46fad3f3b49433f88bd56753ddd743797
Willian van der Velde <willian@pythonheads.nl>
2014-05-26 16:41:00 +0200

    Identifier must be unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 # -*- encoding : utf-8 -*-
 
 class User < ActiveRecord::Base
-  validates :identifier, presence: true
-
-  def sync_with_prediction_io
-    Layer::PredictionIo::User.new(self).sync!
-  end
+  validates :identifier, presence: true, uniqueness: true
 end
