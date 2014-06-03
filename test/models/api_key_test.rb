29ddfa22f46ce88bf1236a28027d6bf6d715e493
Willian <willian@pythonheads.nl>
2014-06-03 10:27:48 +0200

    power api keys introduction

--- test/models/api_key_test.rb
+++ test/models/api_key_test.rb
@@ -2,11 +2,23 @@
 require 'test_helper'
 
 class ApiKeyTest < ActiveSupport::TestCase
-  test 'a valid api key is given' do
-    assert ApiKey.key? 'ABCDE'
+  class NormalApiKeyTest < ApiKeyTest
+    test 'a valid api key is given' do
+      assert ApiKey.key? 'ABCDE'
+    end
+
+    test 'an invalid api key is given' do
+      refute ApiKey.key? 'NOT_AN_API_KEY'
+    end
   end
 
-  test 'an invalid api key is given' do
-    refute ApiKey.key? 'NOT_AN_API_KEY'
+  class PowerUserApiKeyTest < ApiKeyTest
+    test 'a valid power user api key is given' do
+      assert ApiKey.power_user? 'POWERKEY'
+    end
+
+    test 'an invalid power user api key is given' do
+      refute ApiKey.power_user? 'ABCDE'
+    end
   end
 end
